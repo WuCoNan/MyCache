bd82915bbc5cae77f948c582d31a167190141d95
Wuconan <1206079278@qq.com>
2026-01-11 16:34:43 +0800

    2026-01-11

--- client/client.go
+++ client/client.go
@@ -1,1 +1,77 @@
 package client
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/WuCoNan/MyCache/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	conn      *grpc.ClientConn
+	client    pb.ProxyServiceClient
+	proxyAddr string
+}
+
+func NewClient(addr string) *Client {
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Printf("Failed to connect to proxy server: %v", err)
+		return nil
+	}
+	return &Client{
+		conn:      conn,
+		client:    pb.NewProxyServiceClient(conn),
+		proxyAddr: addr,
+	}
+}
+
+func (c *Client) Get(groupName string, key string) ([]byte, bool) {
+	resp, err := c.client.Get(context.TODO(), &pb.GetRequest{
+		GroupName: groupName,
+		Key:       key,
+	})
+	if err != nil {
+		log.Printf("Get request failed: %v", err)
+		return nil, false
+	}
+	return resp.Value, resp.Flag
+}
+
+func (c *Client) Set(groupName string, key string, value []byte) bool {
+	resp, err := c.client.Set(context.TODO(), &pb.SetRequest{
+		GroupName: groupName,
+		Key:       key,
+		Value:     value,
+	})
+	if err != nil {
+		log.Printf("Set request failed: %v", err)
+		return false
+	}
+	return resp.Flag
+}
+
+func (c *Client) Delete(groupName string, key string) bool {
+	resp, err := c.client.Delete(context.TODO(), &pb.DeleteRequest{
+		GroupName: groupName,
+		Key:       key,
+	})
+	if err != nil {
+		log.Printf("Delete request failed: %v", err)
+		return false
+	}
+	return resp.Flag
+}
+
+func (c *Client) AddGroup(groupName string) bool {
+	resp, err := c.client.AddGroup(context.TODO(), &pb.AddGroupRequest{
+		GroupName: groupName,
+	})
+	if err != nil {
+		log.Printf("AddGroup request failed: %v", err)
+		return false
+	}
+	return resp.Flag
+}
--- client/main/main.go
+++ client/main/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"log"
+
+	"github.com/WuCoNan/MyCache/client"
+)
+
+func main() {
+	c := client.NewClient("127.0.0.1:9090")
+	if c == nil {
+		return
+	}
+
+	groupName := "testGroup"
+	if ok := c.AddGroup(groupName); !ok {
+		return
+	}
+	key := "exampleKey"
+	value := []byte("exampleValue")
+	if ok := c.Set(groupName, key, value); !ok {
+		return
+	}
+	val, ok := c.Get(groupName, key)
+	log.Printf("Get key: %s, value: %s, success: %v", key, string(val), ok)
+
+	if ok := c.Delete(groupName, key); !ok {
+		log.Printf("Failed to delete key: %s", key)
+		return
+	}
+}
